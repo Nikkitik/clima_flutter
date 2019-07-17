d2274f163e12179359e40696ff8e81e6dc69098c
nikita.romantsov <nikkitik21@gmail.com>
2019-07-17 16:46:24 +0300

    реализовал поиск погоды по городу

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,15 @@ const apiKey = 'b6906160527fa185018b4e6b0114cdf9';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
